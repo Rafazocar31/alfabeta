8b64496e90543e5b418228dd32f27dbbb18ca231
Leandro Barone <web@leandrobarone.com.ar>
2019-08-14 13:22:53 -0300

    Comillitas lindas

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ for line in lineas_dat:
 	line = line.strip()
 
 	troquel = line[:7]
-	nombre = line[7:44].strip()
-	presentacion = line[44:68].strip()
+	nombre = line[7:51].strip()
+	presentacion = line[51:75].strip()
 	
-	print(troquel)
-	print(nombre)
-	print(presentacion)
+	print(f'El troquel es: "{troquel}"')
+	print(f'El nombre es: "{nombre}"')
+	print(f'La presentación es: "{presentacion}"')
 
 	sys.exit()

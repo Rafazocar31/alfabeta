11e7b884a6a35e77aa76b32916775262049c15b8
Leandro Barone <web@leandrobarone.com.ar>
2019-08-16 13:31:34 -0300

    Exportar a CSV

--- main.py
+++ main.py
@@ -1,52 +1,72 @@
 import sys
-import datetime 
+import datetime
+import csv
 
-def importarArchivosDeAlfabeta(ruta_manual, ruta_manextra):
-	archivo_dat = open(ruta_manual, encoding="ansi")
-	archivo_extra = open(ruta_manextra)
+def importarDrogasDeAlfabeta(ruta_manual, ruta_manextra):
+	drogas = {}
+
+	archivo_dat = open(ruta_manual, encoding='ansi')
+	archivo_extra = open(ruta_manextra, encoding='ansi')
 
 	lineas_dat = archivo_dat.readlines()
+	lineas_extra = archivo_extra.readlines()
 
-	for line in lineas_dat:
-		line = line.strip()
+	#procesamos manual.dat
+	for linea in lineas_dat:
+		linea = linea.strip()
 
-		troquel = line[:7]
-		nombre = line[7:51].strip()
-		presentacion = line[51:75].strip()
-		Laboratorio = line[85:101].strip()
-		precio = line[105:110].strip()
+		fecha_de_vigencia = linea[110:118].strip()
+		fecha_de_vigencia = datetime.datetime.strptime(fecha_de_vigencia, '%Y%m%d')
+		fecha_de_vigencia = fecha_de_vigencia.strftime('%d/%m/%Y')
+
+		precio = linea[105:110].strip()
 		precio = float(precio) / 100
-		fechavc = line[110:118].strip()
-		fechavc = datetime.datetime.strptime(fechavc, '%Y%m%d')
-		fechavc = fechavc.strftime('%d/%m/%Y')
 
-		importado = line[119:120].strip()
-		tipoventa = line[120:121].strip()
-		iva = line[122:123].strip()
-		codlab = line[123:126].strip()
-		regmed = line[126:131].strip()
-		codbarra = line[132:145].strip()
-		unidades = line[147:149].strip()
+		regmed = int(linea[126:131].strip())
 
-		print(f'El troquel es: "{troquel}"')
-		print(f'El nombre es: "{nombre}"')
-		print(f'La presentación es: "{presentacion}"')
-		print(f'El Laboratorio es: "{Laboratorio}"')
-		print(f'El precio es: "{precio}"')
-		print(f'La fecha vigente de precio es: "{fechavc}"')
-		print(f'La importacion es:"{importado}"')
-		print(f'El tipo de venta es:"{tipoventa}"')
-		print(f'El iva es:"{iva}"')
-		print(f'El codigo de laboratorio es:"{codlab}"')
-		print(f'El registro medico es:"{regmed}"')
-		print(f'El codigo de barra es:"{codbarra}"')
-		print(f'Las unidades son:"{unidades}"')
-		
+		drogas[regmed] = {}
 
+		drogas[regmed]['troquel'] = linea[:7].strip()
+		drogas[regmed]['nombre'] = linea[7:51].strip()
+		drogas[regmed]['presentacion'] = linea[51:75].strip()
+		drogas[regmed]['laboratorio'] = linea[85:101].strip()
+		drogas[regmed]['precio'] = precio
+		drogas[regmed]['fecha_de_vigencia'] = fecha_de_vigencia
+		drogas[regmed]['importado'] = linea[119:120].strip()
+		drogas[regmed]['tipoventa'] = linea[120:121].strip()
+		drogas[regmed]['iva'] = linea[122:123].strip()
+		drogas[regmed]['cod_laboratorio'] = linea[123:126].strip()
+		drogas[regmed]['codigo_de_barras'] = linea[132:145].strip()
+		drogas[regmed]['unidades'] = linea[147:149].strip()
+	
+	# procesamos manextra.txt
+	for linea in lineas_extra:
+		linea = linea.strip()
 
-		sys.exit()
+		regmed = int(linea[:5])
+
+		drogas[regmed]['cod_accion_farmacologica'] = linea[9:12].strip()
+		drogas[regmed]['cod_monodroga'] = linea[12:16].strip()
+		drogas[regmed]['cod_formas'] = linea[20:22].strip()
+		drogas[regmed]['potencia'] = linea[22:38].strip()
+		drogas[regmed]['cod_unidad_potencia'] = linea[38:43].strip()
+		drogas[regmed]['cod_tipo_unidad'] = linea[43:48].strip()
+		drogas[regmed]['cod_via'] = linea[48:53].strip()
+	
+	return drogas
+
+def grabarDrogasDeAlfabetaAArchivo(drogas, ruta_output):
+	archivo_output = open(ruta_output, 'w', encoding='ansi', newline='')
+
+	columnas_csv = ['troquel', 'nombre', 'presentacion', 'laboratorio', 'precio', 'fecha_de_vigencia', 'importado', 'tipoventa', 'iva', 'cod_laboratorio', 'codigo_de_barras', 'unidades', 'cod_accion_farmacologica', 'cod_monodroga', 'cod_formas', 'potencia', 'cod_unidad_potencia', 'cod_tipo_unidad', 'cod_via']
+
+	grabador_de_csv = csv.DictWriter(archivo_output, fieldnames=columnas_csv, delimiter=';')
+	grabador_de_csv.writeheader()
+	for droga in drogas:
+		grabador_de_csv.writerow(drogas[droga])
 
 def app():
-	archivos_alfabeta = importarArchivosDeAlfabeta("archivos_alfabeta/manual.dat", "archivos_alfabeta/manextra.txt")
+	drogas = importarDrogasDeAlfabeta('archivos_alfabeta/manual.dat', 'archivos_alfabeta/manextra.txt')
+	grabarDrogasDeAlfabetaAArchivo(drogas, 'output.csv')
 	
 app()
